dfb00581ebb597ccf17c1afca403774e922fcf0d
Alexander Pineda <correopiineda@gmail.com>
2020-01-13 15:12:56 -0500

    Punto de Control

--- ajax/valoresRef.js
+++ ajax/valoresRef.js
@@ -1,4 +1,13 @@
 (()=>{
     const filasCuerpo = document.querySelector("filasCuerpoTabla");
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log("conexion ajax exitosa a archivo de eliminacion de registros");
+        }
+    };
+    xmlhttp.open("POST", "./php/eliminarRegistros.php", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send(envio);
     
 })()
